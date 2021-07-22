82e07458bfdc5a4bd56ddf73a224cdaeab9e0869
DESKTOP-UAAKPIM\pravi <pravin999.s@gmail.com>
2021-07-22 12:01:12 +0530

    First commit

---
+++ eclipse-workspace/gitdemoproject1/src/gitdemoproject1/test.java
@@ -0,0 +1,14 @@
+package gitdemoproject1;
+
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			// TODO Auto-generated method stub
+			String s=" varsha ";
+			System.out.println(s);
+
+	}
+
+}
